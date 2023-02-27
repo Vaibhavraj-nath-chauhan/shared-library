e2167c0b80c7945b70910e4a81d928484c1b73ba
harshit-chaubisa <114575164+harshit-chaubisa@users.noreply.github.com>
2023-02-27 19:10:59 +0530

    Update terrafrm.groovy

--- vars/terrafrm.groovy
+++ vars/terrafrm.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 
-def call(Map config, String action="init"){
+def call(Map config, def action="init"){
 
     def secretToken = config.secretToken ?:error("No token Provided")
     def awsProfile = config.awsProfile ?: error("No prfile provided")
@@ -17,14 +17,15 @@ def call(Map config, String action="init"){
                     """,
                     returnStdout: true
                 ).trim()
-        if (action == "init"){
-         sh "set +x; docker run --rm ${AWS_CREDENTIALS} -e AWS_PROFILE=${awsProfile} -e AWS_REGION=${awsRegion} -v ${workspace}/:/code -w ${workDir} ${terraformDockerImage} terraform init"   
-        }
-        else if(action == "plan"){
-             sh "set +x; docker run --rm ${AWS_CREDENTIALS} -e AWS_PROFILE=${awsProfile} -e AWS_REGION=${awsRegion} -v ${workspace}/:/code -w ${workDir} ${terraformDockerImage} terraform plan -var image_number=${imgTag}"
-        }
-        else (action == "apply"){
-             //sh "set +x; docker run --rm ${AWS_CREDENTIALS} -e AWS_PROFILE=accel -e AWS_REGION=us-east-1 -v ${workspace}/:/code -w /code/accel-ec2/services-prod/website-comparison msshahanshah/tools:terrform01 terraform apply -var image_number=${img_tag}"
-        }    
+//         if (action == "init"){
+//          sh "set +x; docker run --rm ${AWS_CREDENTIALS} -e AWS_PROFILE=${awsProfile} -e AWS_REGION=${awsRegion} -v ${workspace}/:/code -w ${workDir} ${terraformDockerImage} terraform init"   
+//         }
+//         else if(action == "plan"){
+//              sh "set +x; docker run --rm ${AWS_CREDENTIALS} -e AWS_PROFILE=${awsProfile} -e AWS_REGION=${awsRegion} -v ${workspace}/:/code -w ${workDir} ${terraformDockerImage} terraform plan -var image_number=${imgTag}"
+//         }
+//         else (action == "apply"){
+//              //sh "set +x; docker run --rm ${AWS_CREDENTIALS} -e AWS_PROFILE=accel -e AWS_REGION=us-east-1 -v ${workspace}/:/code -w /code/accel-ec2/services-prod/website-comparison msshahanshah/tools:terrform01 terraform apply -var image_number=${img_tag}"
+//         }  
+        sh "set +x; docker run --rm ${AWS_CREDENTIALS} -e AWS_PROFILE=${awsProfile} -e AWS_REGION=${awsRegion} -v ${workspace}/:/code -w ${workDir} ${terraformDockerImage} terraform ${action}"   
         }
 }
